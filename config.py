e3e551cf2b257361c3b4a6ac48cc2d9ad74bbf51
pg-sys <46448885+pg-sys@users.noreply.github.com>
2023-07-21 12:49:47 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+#ASTRA DB
+config_astra_db_token_id=<<your astra token user>>
+config_astra_db_token_password=<<your astra token password>>
+config_astra_db_keyspace='mykeyspace'
+config_astra_db_vector_tablename='vectorizeddocuments'
+config_astra_db_secure_connect_bundle_path=<<your astra secure connect bundle>
+
+#OPEN AI
+config_open_ai_secret=<<your openAI key>>
+
+#custom parameters
+config_inputdir='./documentsinput/'
+config_outputdir='./documentsoutput/'
